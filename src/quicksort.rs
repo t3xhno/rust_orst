c0b769dec552bf23bed0a2325559531b7a9b9f00
t3x <t3xhnolainx@gmail.com>
2021-11-05 17:30:55 +0100

    orst | QuickSort trash

--- src/quicksort.rs
+++ src/quicksort.rs
@@ -2,29 +2,56 @@ use super::Sorter;
 
 pub struct QuickSort;
 
+fn quicksort<T: Ord>(slice: &mut [T]) {
+    match slice.len() {
+        0 | 1 => return,
+        2 => {
+            if slice[0] > slice[1] {
+                slice.swap(0, 1);
+            }
+            return;
+        },
+        _ => (),
+    }
+    let (pivot, rest) = slice.split_first_mut().expect("Slice is not empty!");
+    let mut left = 0;
+    let mut right = rest.len() - 1;
+    while left <= right {
+        if &rest[left] <= pivot {
+            left += 1;
+        } else if &rest[right] > pivot {
+            if right == 0 {
+                break;
+            }
+            right -= 1;
+        } else {
+            rest.swap(left, right);
+            left += 1;
+            if right == 0 {
+                break;
+            }
+            right -= 1;
+        }
+    }
+    let left = left + 1;
+    slice.swap(0, left - 1);
+    let (left, right) = slice.split_at_mut(left - 1);
+    quicksort(left);
+    quicksort(&mut right[1..]);
+}
+
 impl Sorter for QuickSort {
     fn sort<T>(&self, slice: &mut [T])
     where
         T: Ord,
     {
-        for unsorted in 0..slice.len() {
-            let smallest_in_rest = slice[unsorted..]
-                .iter()
-                .enumerate()
-                .min_by_key(|&(_, v)| v)
-                .map(|(i, _)| unsorted + i)
-                .expect("Slice is non-empty.");
-
-            if unsorted != smallest_in_rest {
-                slice.swap(unsorted, smallest_in_rest);
-            }
-        }
+        quicksort(slice);
     }
 }
 
 #[test]
 fn quick_works() {
-    let mut things = vec![4, 2, 3, 1];
+    let mut things = vec![4, 2, 3, 1, 5];
     QuickSort.sort(&mut things);
-    assert_eq!(things, &[1, 2, 3, 4]);
+    assert_eq!(things, &[1, 2, 3, 4, 5]);
 }
